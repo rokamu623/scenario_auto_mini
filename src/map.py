39d8ff4f2d1380e06cf4a9c0295ba494708f4ec7
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2024-03-26 20:16:14 +0900

    feat: search crossroad
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- src/map.py
+++ src/map.py
@@ -2,7 +2,17 @@ class Map:
     def __init__(self, map_file_path: str):
         map_data = open(map_file_path, 'r')
         self._map_str = map_data.read()
+        map_data = open(map_file_path, 'r')
         self._map = [[_ for _ in r.replace('\n', '')] for r in map_data.readlines()]
+
+    def search_crossroad(self) -> tuple[int, int]:
+        try:
+            y = ['x' in r for r in self._map].index(True)
+            x = self._map[y].index('x')
+        except:
+            raise ValueError('crossroad not found.')
+
+        return x, y
 
     def display(self):
         print(self._map_str)
